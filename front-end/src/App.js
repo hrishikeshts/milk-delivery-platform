c1c2d85389fa6c1029866ec52d2fbc270daf9754
Hrishikesh Thesserikaran <hrishikeshts@gmail.com>
2021-05-23 18:49:20 +0530

    Add mobile view & splash screen

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,23 +1,29 @@
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
+import TitleSVG from './TitleSVG';
 import './App.css';
 
 function App() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 3000)
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Container">
+      <div className="App">
+        {loading ? 
+            <TitleSVG /> :
+          <>
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>Edit <code>src/App.js</code> and save to reload.</p>
+          </>
+        } 
+      </div>
     </div>
   );
 }
---
+++ front-end/src/TitleSVG.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+export default function TitleSVG() {
+    return (
+        <svg viewBox='0 0 100 100' xmlns='http://www.w3.org/2000/svg'>
+            <text
+                fill='#0078F0'
+                font-family='Comfortaa, cursive'
+                font-weight='700'
+                text-anchor="middle"
+                font-size="16.1"
+                x='50'
+                y='50'
+            >
+                DairyDash
+            </text>
+            <text
+                fill='#0078F0'
+                font-family='Montserrat, sans-serif'
+                font-weight='500'
+                text-anchor="middle"
+                font-size="3.5"
+                letter-spacing="1.75"
+                x='50'
+                y='57'
+            >
+                MILK DELIVERY PLATFORM
+            </text>
+        </svg>
+    );
+}
