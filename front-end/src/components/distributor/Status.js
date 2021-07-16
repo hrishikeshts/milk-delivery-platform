e1ffb2c4ac8482b34de5f05452ada7caccdd9b1d
Kevinmt24 <kevinmt01@gmail.com>
2021-07-17 01:34:53 +0530

    added Circular Progress Indicator

--- front-end/src/components/distributor/Status.js
+++ front-end/src/components/distributor/Status.js
@@ -1,15 +1,48 @@
 import { Link } from "react-router-dom";
 import { IoIosArrowForward } from "react-icons/io";
+import { CircularProgressbar, CircularProgressbarWithChildren, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 export default function Status({ products, retailerCount, orders, orderCount }) {
+
+    const percentage = 66;
     return (
         <>
             <h3 className="blue">Next Delivery Info</h3>
-            <div className="chart-box bg-white login-form">
+            {/* <div className="chart-box bg-white login-form">
                 <div>Total Retailers: {retailerCount.total}</div>
                 <div>Retailers confirmed: {orders.length / products.length}</div>
                 <div>Retailers ordered: {retailerCount.ordered}</div>
                 <div>Total Count: {orderCount.total}</div>
+            </div> */}
+            <div style={{marginLeft: 100,width: 200, height: 200}}>
+    
+                <CircularProgressbarWithChildren
+                    value={66}
+                    styles={buildStyles({
+                        pathColor: "#0080FF",
+                        trailColor: "white",
+                        pathTransitionDuration: 0.5,
+                    })}
+                > 
+                    
+                    {/* Foreground path */}
+                    <CircularProgressbarWithChildren
+                        value={44}
+                        styles={buildStyles({
+                            pathColor:"#003163",
+                            trailColor: "transparent",
+                            pathTransitionDuration: 0.5,
+                        })}
+                    >
+                    <div>
+                        <h2 className="m-0">33</h2>
+                        <h6>retailers confirmed</h6>
+                        <h2 className="m-0">22</h2>
+                        <h6>retailers ordered</h6>
+                    </div>
+                </CircularProgressbarWithChildren>
+                </CircularProgressbarWithChildren>
             </div>
             <div className="items-container">
                 {products.map((product, key) => {
