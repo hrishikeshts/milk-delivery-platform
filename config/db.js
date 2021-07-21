d9c799af4c413d7b0090c76dc171fff3b2b79078
Hrishikesh Thesserikaran <hrishikeshts@gmail.com>
2021-07-21 17:52:05 +0530

    Update db config and package.json

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mysql = require("mysql");
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
     host: process.env.DB_HOST || "us-cdbr-east-04.cleardb.com",
     user: process.env.DB_USER || "b83601a0777799",
     password: process.env.DB_PASS || "aac4744b",
@@ -8,12 +8,13 @@ const db = mysql.createConnection({
     multipleStatements: true,
 });
 
-db.connect((err) => {
-    if (err) {
-        throw err;
-    }
-    console.log("MySQL connected...");
-});
+// db.connect((err) => {
+//     if (err) {
+//         throw err;
+//     } else {
+//         console.log("MySQL connected...");
+//     }
+// });
 
 const queryPromise = async (sql, args) => {
     return new Promise((resolve, reject) => {
