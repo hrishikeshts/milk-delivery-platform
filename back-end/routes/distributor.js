ed1bec957333ad9a6f0fab28358868280b742e4c
Hrishikesh Thesserikaran <hrishikeshts@gmail.com>
2021-07-21 18:16:52 +0530

    First commit of the branch

--- back-end/routes/distributor.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { distributorStatus, setDelivery } = require("../controllers/distributor");
-
-const router = express.Router();
-
-router.get("/d:did/status", distributorStatus);
-router.post("/d/o:oid/delivery", setDelivery);
-
-module.exports = router;
